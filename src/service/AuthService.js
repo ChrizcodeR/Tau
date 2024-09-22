6824d043536654e07f0039e57ca7e8afcf1df82e
DISTRIFABRICA-RAMIREZ <desarrollodistrifabrica@gmail.com>
2024-09-21 23:12:04 -0500

    update into attendanceservice and Authservice

--- src/service/AuthService.js
+++ src/service/AuthService.js
@@ -1,15 +1,13 @@
 import api from './api';
 
+// Declarar las credenciales aquí
+const email = "angeldev@gmail.com";
+const password = "12345678";
+
 const AuthService = {
-
-  login: async (email, password) => {
-
-    // Enviar las credenciales y obtener el token
-    const email = "angeldev@gmail.com";
-    const password = "12345678";
-
+  login: async () => {
     const response = await api.post('/login', { email, password });
-
+    console.log("Datos enviados:", JSON.stringify(response)); // validar que esta llegando
     // Guardar el token en el localStorage
     if (response.data.status) {
       localStorage.setItem('authToken', response.data.token);
@@ -34,5 +32,18 @@ const AuthService = {
   },
 };
 
-// Exportar el servicio de autenticación
+// Llama al método de inicio de sesión
+const loginAndUseToken = async () => {
+  try {
+    const token = await AuthService.login();
+    console.log('Token:', token);
+    // Aquí puedes usar el token para las solicitudes de AttendanceService
+  } catch (error) {
+    console.error('Error during login:', error.message);
+  }
+};
+
+// Llama a la función de inicio de sesión
+loginAndUseToken();
+
 export default AuthService;
