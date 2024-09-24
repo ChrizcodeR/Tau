0857379c82bd586ef2cbf5a5cfb2f7e19ab98114
DISTRIFABRICA-RAMIREZ <desarrollodistrifabrica@gmail.com>
2024-09-24 12:29:26 -0500

    update in line 38 and 44, i created a const with convert info at json format

--- src/service/AttendanceService.js
+++ src/service/AttendanceService.js
@@ -21,24 +21,22 @@ export const AttendanceService = {
 
   validateAndCreateArrival: async (codigo_tr, requestData) => {
     try {
-
-      console.log("Datos recibidos",codigo_tr, requestData);
-
+      console.log("Datos recibidos", codigo_tr, requestData);
+  
       // Lista de empleados del AttendanceService
-      const response = await AttendanceService.getEmployees();
-      // Accede a los empleados desde la respuesta
-      const employees = response.empleados;
+      const employees = await AttendanceService.getEmployees(); // Cambiar aquí
+      console.log("Respuesta de empleados:", employees); // Verifica la respuesta
+  
       const employee = employees.find(
         (emp) => emp.codigo_empleado === codigo_tr
       );
-
+  
       if (employee) {
         console.log("Empleado válido:", employee);
-
+        
         const requestDataJSON = JSON.stringify(requestData);
-        // Ver datos que se enviarán
-        console.log("Datos a enviar a registrar:", JSON.stringify(requestData, null, 2));
-
+        console.log("Datos a enviar a registrar:", requestDataJSON);
+  
         // Registrar la asistencia
         const token = AuthService.getToken();
         const response = await api.post("/asistencia/registrar", requestDataJSON, {
@@ -51,12 +49,7 @@ export const AttendanceService = {
         throw new Error("El código no existe");
       }
     } catch (error) {
-      console.error(
-        "Error en la validación y creación de llegada:",
-        error.message
-      );
-      throw error; // Emvia el error para manejarlo en el componente
+      console.error("Error en la validación y creación de llegada:", error.message);
+      throw error; // Envía el error para manejarlo en el componente
     }
   },
-};
-
